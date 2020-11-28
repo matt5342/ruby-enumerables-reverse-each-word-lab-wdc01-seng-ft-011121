9f72147eab9dcf46fcde24be0c4f689a7788e543
Matthew Sewell <mattfsewell@gmail.com>
2020-11-28 15:29:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word (string)
   new_array = []
   array = %w(string)
-  array.each do element.reverse   
+  array.each do |element|
+      element.reverse   
       new_array << element
   end
   new_string = new_array.join(' ')
